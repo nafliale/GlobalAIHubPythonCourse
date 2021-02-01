bd91fc9ca472be30532ca1e463249c8b85ecf977
nafliale <78366610+nafliale@users.noreply.github.com>
2021-02-01 19:36:25 -0300

    Add files via upload

---
+++ Homeworks/GlobalAIHubPythonCourse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#the "for ... in range" is used to output a value or characters in a specified range
+
+
+# In[3]:
+
+
+for x in range (2, 6):
+    print (x)
+
+
+# In[ ]:
+
+
+
+
